a844ba728b687bcab6726c4caeb34376c1408d4e
Josué Natanael Paz <jpaz@xytech.com.ar>
2021-09-02 22:49:36 -0300

    avance botones y footer

---
+++ js/scripts.js
@@ -0,0 +1,116 @@
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+
+
+
+
+//Boton top -----------------------------------------------------------------------------------------
+
+
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function() {
+        closeAllForms();
+        $("html, body").animate({ scrollTop: 0 }, 600);
+
+        return false;
+    });
+});
+
+// SCROLL TOP
+if (btn1.classList.contains("selected")) {
+    $("html, body").animate({ scrollTop: -1000 }, 2000);
+}
+
+
+
+
+
+// MENU SELECCIONADO
+
+btn1.addEventListener('click', () => {
+    if (!btn1.classList.contains("selected")) {
+        btn1.classList.toggle("selected");
+    }
+
+    if (btn2.classList.contains("selected")) {
+        btn2.classList.toggle("selected");
+    }
+
+    if (btn3.classList.contains("selected")) {
+        btn3.classList.toggle("selected");
+    }
+
+    if (btn4.classList.contains("selected")) {
+        btn4.classList.toggle("selected");
+    }
+
+});
+
+btn2.addEventListener('click', () => {
+    if (!btn2.classList.contains("selected")) {
+        btn2.classList.toggle("selected");
+    }
+
+    if (btn1.classList.contains("selected")) {
+        btn1.classList.toggle("selected");
+    }
+
+    if (btn3.classList.contains("selected")) {
+        btn3.classList.toggle("selected");
+    }
+
+    if (btn4.classList.contains("selected")) {
+        btn4.classList.toggle("selected");
+    }
+
+});
+
+btn3.addEventListener('click', () => {
+    if (!btn3.classList.contains("selected")) {
+        btn3.classList.toggle("selected");
+    }
+
+    if (btn1.classList.contains("selected")) {
+        btn1.classList.toggle("selected");
+    }
+
+    if (btn2.classList.contains("selected")) {
+        btn2.classList.toggle("selected");
+    }
+
+    if (btn4.classList.contains("selected")) {
+        btn4.classList.toggle("selected");
+    }
+
+});
+
+btn4.addEventListener('click', () => {
+    if (!btn4.classList.contains("selected")) {
+        btn4.classList.toggle("selected");
+    }
+
+    if (btn2.classList.contains("selected")) {
+        btn2.classList.toggle("selected");
+    }
+
+    if (btn3.classList.contains("selected")) {
+        btn3.classList.toggle("selected");
+    }
+
+    if (btn1.classList.contains("selected")) {
+        btn1.classList.toggle("selected");
+    }
+
+});
+
+// MENU SELECCIONADO FIN
