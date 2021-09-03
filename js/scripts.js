00ac93fe5f94613fd4586b8832ef2f473656d229
Josué Natanael Paz <jpaz@xytech.com.ar>
2021-09-03 00:10:55 -0300

    avance
    avance

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
 const btn3 = document.getElementById("btn3");
 const btn4 = document.getElementById("btn4");
-
+const btn5 = document.getElementById("btn5");
 
 
 
@@ -53,6 +53,9 @@ btn1.addEventListener('click', () => {
     if (btn4.classList.contains("selected")) {
         btn4.classList.toggle("selected");
     }
+    if (btn5.classList.contains("selected")) {
+        btn5.classList.toggle("selected");
+    }
 
 });
 
@@ -71,6 +74,10 @@ btn2.addEventListener('click', () => {
 
     if (btn4.classList.contains("selected")) {
         btn4.classList.toggle("selected");
+    }
+
+    if (btn5.classList.contains("selected")) {
+        btn5.classList.toggle("selected");
     }
 
 });
@@ -92,11 +99,19 @@ btn3.addEventListener('click', () => {
         btn4.classList.toggle("selected");
     }
 
+    if (btn5.classList.contains("selected")) {
+        btn5.classList.toggle("selected");
+    }
+
 });
 
 btn4.addEventListener('click', () => {
     if (!btn4.classList.contains("selected")) {
         btn4.classList.toggle("selected");
+    }
+
+    if (btn1.classList.contains("selected")) {
+        btn1.classList.toggle("selected");
     }
 
     if (btn2.classList.contains("selected")) {
@@ -107,8 +122,31 @@ btn4.addEventListener('click', () => {
         btn3.classList.toggle("selected");
     }
 
+    if (btn5.classList.contains("selected")) {
+        btn5.classList.toggle("selected");
+    }
+
+});
+
+btn5.addEventListener('click', () => {
+    if (!btn5.classList.contains("selected")) {
+        btn5.classList.toggle("selected");
+    }
+
     if (btn1.classList.contains("selected")) {
         btn1.classList.toggle("selected");
+    }
+
+    if (btn2.classList.contains("selected")) {
+        btn2.classList.toggle("selected");
+    }
+
+    if (btn3.classList.contains("selected")) {
+        btn3.classList.toggle("selected");
+    }
+
+    if (btn4.classList.contains("selected")) {
+        btn4.classList.toggle("selected");
     }
 
 });
